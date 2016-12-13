f6ef5440ba5c2d9aa10354ab766b95475805e8fa
IkarasI <ikarasi_tukasa@yse-c.net>
2016-12-13 14:51:15 +0900

    add aloha.java10

--- 20161213/Aloha.java
+++ 20161213/Aloha.java
@@ -1,14 +1,16 @@
 public class Aloha {
 	public static void main(String[] args) {
-		System.out.print("main :");
-		String msg = new java.util.Scanner(System.in).nextLine();
-		System.out.print("sub :");
-		String msg2 = new java.util.Scanner(System.in).nextLine();
-		for (int i=0;i<20 ;i++ ) {
-			if (i%3==0) {
-				System.out.println(msg2);
-			}else{
-				System.out.println(msg);
+		while (true) {
+			System.out.print("main :");
+			String msg = new java.util.Scanner(System.in).nextLine();
+			System.out.print("sub :");
+			String msg2 = new java.util.Scanner(System.in).nextLine();
+			for (int i=0;i<20 ;i++ ) {
+				if (i%3==0) {
+					System.out.println(msg2);
+				}else{
+					System.out.println(msg);
+				}
 			}
 		}
 	}
