72d42d7975aef5c039110a2a469060fb55526f86
IkarasI <ikarasi_tukasa@yse-c.net>
2016-12-13 14:43:50 +0900

    add aloha.java6

--- 20161213/Aloha.java
+++ 20161213/Aloha.java
@@ -1,7 +1,11 @@
 public class Aloha {
 	public static void main(String[] args) {
 		for (int i=0;i<20 ;i++ ) {
-		System.out.println("ALOHA!"+i);
+			if (i%3==0) {
+				System.out.println("アロハ！");
+			}else{
+				System.out.println("ALOHA!");
+			}
 		}
 	}
 }
