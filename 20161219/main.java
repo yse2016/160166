c37692f1dd6a2a3a287289deb9d99bfdc7601454
IkarasI <ikarasi_tukasa@yse-c.net>
2016-12-19 12:12:39 +0900

    add 20161219file

---
+++ 20161219/main.java
@@ -0,0 +1,27 @@
+import  java.awt.FlowLayout;
+import javax.swing.*;
+public class main{
+	public static void main(String[] args) {
+		JFrame frame = new JFrame("GUItest");
+		JLabel label = new JLabel("Hello world");
+		JButton button = new JButton("push");
+		JLabel label2 = new JLabel("Aloha");
+		JButton button2 = new JButton("push!");
+		JLabel label3 = new JLabel("Aloha!");
+		JButton button3 = new JButton("push!!");
+		JLabel label4 = new JLabel("ALOHA");
+		JButton button4 = new JButton("pUuuuuuuuush!!!");
+		frame.getContentPane().setLayout(new FlowLayout());
+		frame.getContentPane().add(button);
+		frame.getContentPane().add(label);
+		frame.getContentPane().add(button2);
+		frame.getContentPane().add(label2);
+		frame.getContentPane().add(button3);
+		frame.getContentPane().add(label3);
+		frame.getContentPane().add(button4);
+		frame.getContentPane().add(label4);
+		frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+		frame.setSize(300,100);
+		frame.setVisible(true);
+	}
+}
