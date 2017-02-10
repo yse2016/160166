cb9cbbfc16983b3a0826e63320a22d8da3f87f94
IkarasI <ikarasi_tukasa@yse-c.net>
2017-02-10 14:06:59 +0900

    proto update

--- 20170209/slot.java
+++ 20170209/slot.java
@@ -16,9 +16,9 @@ class SlotMan implements ActionListener{
 	JButton btnIns;
 	JTextArea textArea;
 	JScrollPane scrollPane;
+	JButton btnRnkup;
+	JButton btnRnkdun;
 
-		JButton btnRnkup;
-		JButton btnRnkdun;
 
 		Random rnd = new Random();
 		int coin =1000;
@@ -35,13 +35,25 @@ class SlotMan implements ActionListener{
 			int ranC2=8;
 			int ranC3=9;
 
+	int i;
+	JButton btnOpen;
+	JButton btnSave;
+	FileReader fr;
+	BufferedReader br;
+	FileWriter fw;
+	PrintWriter pw;
+    JFileChooser filechooser = new JFileChooser();
+
 	public SlotMan(){
 		frame = new JFrame("SlotMachine");
 		frame.setLocation(400, 400);
-		frame.setSize(240, 300);
+		frame.setSize(400, 200);
 		frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
 
 		fileName = new JTextField("please press INSERT BUTTON",20);
+		btnOpen=new JButton("LOAD");
+		btnSave=new JButton("SAVE");
+
 		btnSpin=new JButton("SPIN");
 		btnIns=new JButton("INSERT");
 		btnRnkdun=new JButton("RankDOWN");
@@ -55,19 +67,27 @@ class SlotMan implements ActionListener{
 		fieldpanel.add(btnIns);
 			fieldpanel.add(btnRnkdun);
 			fieldpanel.add(btnRnkup);
-		btnSpin.setActionCommand("spin");
-		btnSpin.addActionListener(this);
-		btnIns.setActionCommand("insert");
-		btnIns.addActionListener(this);
-			btnRnkup.setActionCommand("up");
-			btnRnkup.addActionListener(this);
-			btnRnkdun.setActionCommand("down");
-			btnRnkdun.addActionListener(this);
+		btnOpen.setActionCommand("open");
+		btnOpen.addActionListener(this);
+		btnSave.setActionCommand("save");
+		btnSave.addActionListener(this);
+			btnSpin.setActionCommand("spin");
+			btnSpin.addActionListener(this);
+			btnIns.setActionCommand("insert");
+			btnIns.addActionListener(this);
+				btnRnkup.setActionCommand("up");
+				btnRnkup.addActionListener(this);
+				btnRnkdun.setActionCommand("down");
+				btnRnkdun.addActionListener(this);
 
 		Container con = frame.getContentPane();
 		con.setLayout(new GridLayout(2, 1));
 		con.add(fieldpanel);
 		con.add(scrollPane);
+
+		fieldpanel.add(btnOpen);
+		fieldpanel.add(btnSave);
+
 		fieldpanel.add(btnSpin);
 		fieldpanel.add(btnIns);
 		fieldpanel.add(btnRnkup);
@@ -133,6 +153,61 @@ class SlotMan implements ActionListener{
 				dice -=1;
 			}else{
 				textArea.setText("MINIMUM Rank");
+			}
+		}else if (cmd.equals("open")) {
+		String textFileName = fileName.getText();
+
+			JFileChooser fc=new JFileChooser();
+			fc.setCurrentDirectory(new File("."));
+			int ret=fc.showOpenDialog(frame);
+			if(ret == JFileChooser.APPROVE_OPTION) {
+					File file = fc.getSelectedFile();
+					textFileName = file.getAbsolutePath();
+					fileName.setText(textFileName);
+				}
+			try{
+				fr = new FileReader(textFileName);
+				br = new BufferedReader(fr);
+
+				String data;
+				while ((data=br.readLine())!=null) {
+					fileName.setText(data);
+				 i= Integer.parseInt(data);
+				 coin =i;
+					System.out.println(data+"data");
+				}
+				System.out.println(i+"int");
+			}catch (IOException e) {
+				System.out.println("ERROR");
+				e.printStackTrace();
+			}
+		}else if (cmd.equals("save")) {
+			String textFileName = fileName.getText();
+
+			JFileChooser fc=new JFileChooser();
+			fc.setCurrentDirectory(new File("."));
+			int ret=fc.showSaveDialog(frame);
+			if(ret == JFileChooser.APPROVE_OPTION) {
+					File file = fc.getSelectedFile();
+					textFileName = file.getAbsolutePath();
+				}
+			try{
+				fw=new FileWriter(textFileName);
+				pw=new PrintWriter(fw);
+				String data= String.valueOf(coin);
+				pw.println(data);
+
+			}catch (IOException e) {
+				System.out.println("ERROR");
+				e.printStackTrace();
+			} finally {
+				try{
+					fw.close();
+					pw.close();
+				}catch (IOException e) {
+					System.out.println("ERROR");
+					e.printStackTrace();
+				}
 			}
 		}
 		fileName.setText("coin@"+coin+"Chage@"+chage+"Rank"+dice);
